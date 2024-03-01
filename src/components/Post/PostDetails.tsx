deb1bc26c1ac252b2acd742c7f107e6e04fc154b
whytepeter <peteremmanuelwhyte@gmail.com>
2024-03-01 02:48:25 -0800

    udpate

--- src/components/Post/PostDetails.tsx
+++ src/components/Post/PostDetails.tsx
@@ -10,13 +10,19 @@ export default function PostDetails() {
     <div className="w-full max-w-3xl mx-auto bg-background p-4 md:p-6 rounded-3xl flex flex-col gap-5">
       <BackButton />
 
-      <h4 className="font-medium text-dark-900">{post?.name}</h4>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        <DisplayCard title="Username" value="johndoe22" />
-        <DisplayCard title="Email" value="johndoe@gmail.com" />
-        <DisplayCard title="Phone" value="1234567890" />
-        <DisplayCard title="website" value="https://johndoe.com" />
-      </div>
+      {post ? (
+        <>
+          <h4 className="font-medium text-dark-900">{post.name}</h4>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <DisplayCard title="Username" value={post.username} />
+            <DisplayCard title="Email" value={post.email} />
+            <DisplayCard title="Phone" value={post.phone} />
+            <DisplayCard title="website" value={post.website} />
+          </div>
+        </>
+      ) : (
+        <div>Ops no post available</div>
+      )}
     </div>
   );
 }
